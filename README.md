f9b56a9765143d17615425feb3a72a37f2dbaa5c
Jonathan Conway <jonathan.conway@gmail.com>
2014-07-08 22:17:34 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+mi9 Coding Challenge Solution
+=============================
+
+Installation
+------------
+
+1. Install pre-requisites: node, npm.
+
+2. Clone the solution.
+	git clone https://github.com/jonathanconway/mi9-coding-challenge-jonathan
+
+3. Install `express`, etc.
+	sudo npm install
+
+4. Start the server
+	sudo node server
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/', function(req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    try {
+        res.json({
+            response:
+                req.body.payload
+                    .filter(function (payloadItem) {
+                        return payloadItem.image &&
+                                payloadItem.drm &&
+                                payloadItem.episodeCount > 0;
+                    })
+                    .map(function (payloadItem) {
+                        return { 
+                            image: payloadItem.image.showImage, 
+                            slug: payloadItem.slug, 
+                            title: payloadItem.title
+                        };
+                    })
+        });
+    }
+    catch (ex) {
+        res.json(400, { error: 'Could not decode request: ' + ex.message });
+    }
+
+    res.end();
+});
+
+app.listen(80, function () {});
